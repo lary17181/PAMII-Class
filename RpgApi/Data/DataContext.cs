27e7426d3437a4f6255b3c056d8f08f5c1892c5c
unknown <lary.nany17@gmail.com>
2025-04-08 09:12:12 -0300

    Aula 7

--- RpgApi/Data/DataContext.cs
+++ RpgApi/Data/DataContext.cs
@@ -1,14 +1,11 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using Microsoft.EntityFrameworkCore;
+using System.Reflection.Emit;
 using RpgApi.Models;
 using RpgApi.Models.Enuns;
+using RpgApi.Utils;
 
 namespace RpgApi.Data
 {
-    public class DataContext : DbContext
+    public class DataContext : DbContext 
     {
         public DataContext(DbContextOptions<DataContext> options) : base(options)
         {
@@ -17,21 +14,23 @@ namespace RpgApi.Data
 
          public DbSet<Personagem> TB_PERSONAGENS { get; set; }
          public DbSet<Arma> TB_ARMAS { get; set; }
+         public DbSet<Usuario> TB_USARIOS { get; set; }
 
-        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        protected override void OnModelCreating(ModuleBuilder modelBuilder)
         {
             modelBuilder.Entity<Personagem>().ToTable("TB_PERSONAGENS");
             modelBuilder.Entity<Arma>().ToTable("TB_ARMAS");
+            modelBuilder.Entity<Usuario>().ToTable("TB_USUARIOS");
             
             modelBuilder.Entity<Personagem>().HasData
             (
-                new Personagem() { Id = 1, Nome = "Frodo", PontosVida = 100, Forca = 17, Defesa = 23, Inteligencia = 33, Classe = ClasseEnum.Cavaleiro},
-                new Personagem() { Id = 2, Nome = "Sam", PontosVida = 100, Forca = 15, Defesa = 25, Inteligencia = 30, Classe = ClasseEnum.Cavaleiro},
-                new Personagem() { Id = 3, Nome = "Galadriel", PontosVida = 100, Forca = 18, Defesa = 21, Inteligencia = 35, Classe = ClasseEnum.Clerigo},
-                new Personagem() { Id = 4, Nome = "Gandalf", PontosVida = 100, Forca = 18, Defesa = 18, Inteligencia = 37, Classe = ClasseEnum.Mago},
-                new Personagem() { Id = 5, Nome = "Hobbit", PontosVida = 100, Forca = 20, Defesa = 17, Inteligencia = 31, Classe = ClasseEnum.Cavaleiro},
-                new Personagem() { Id = 6, Nome = "Celeborn", PontosVida = 100, Forca = 21, Defesa = 13, Inteligencia = 34, Classe = ClasseEnum.Clerigo},
-                new Personagem() { Id = 7, Nome = "Radagast", PontosVida = 100, Forca = 25, Defesa = 11, Inteligencia = 35, Classe = ClasseEnum.Mago}
+                new Personagem() { Id = 1, Nome = "Frodo", PontosVida = 100, Forca = 17, Defesa = 23, Inteligencia = 33, Classe = ClasseEnum.Cavaleiro, UsuarioId=1},
+                new Personagem() { Id = 2, Nome = "Sam", PontosVida = 100, Forca = 15, Defesa = 25, Inteligencia = 30, Classe = ClasseEnum.Cavaleiro, UsuarioId=1},
+                new Personagem() { Id = 3, Nome = "Galadriel", PontosVida = 100, Forca = 18, Defesa = 21, Inteligencia = 35, Classe = ClasseEnum.Clerigo, UsuarioId=1},
+                new Personagem() { Id = 4, Nome = "Gandalf", PontosVida = 100, Forca = 18, Defesa = 18, Inteligencia = 37, Classe = ClasseEnum.Mago, UsuarioId=1},
+                new Personagem() { Id = 5, Nome = "Hobbit", PontosVida = 100, Forca = 20, Defesa = 17, Inteligencia = 31, Classe = ClasseEnum.Cavaleiro, UsuarioId=1},
+                new Personagem() { Id = 6, Nome = "Celeborn", PontosVida = 100, Forca = 21, Defesa = 13, Inteligencia = 34, Classe = ClasseEnum.Clerigo, UsuarioId=1},
+                new Personagem() { Id = 7, Nome = "Radagast", PontosVida = 100, Forca = 25, Defesa = 11, Inteligencia = 35, Classe = ClasseEnum.Mago, UsuarioId=1}
             );
 
              modelBuilder.Entity<Arma>().HasData
@@ -44,6 +43,28 @@ namespace RpgApi.Data
               new Arma() { Id = 6, Nome = "Foice", Dano = 33 },
               new Arma() { Id = 7, Nome = "Cajado", Dano = 32}
             );
+            Usuario user = new Usuario();
+            Criptografia.CriarPasswordHash("123456", out byte[] hash, out byte[]salt);
+            user.Id=1;
+            user.Username="UsuarioAdmin";
+            user.PasswordString=string.Empty;
+            user.PasswordHash=hash;
+            user.PasswordSalt=salt;
+            user.Perfil="Admin";
+            user.Email="seuEmail@gmail.com";
+            user.Latitude=-23.5200241;
+            user.Longitude=-46.596498;
+            
+            modelBuilder.Entity<Usuario>().HasData(user);
+
+            modelBuilder.Entity<Usuario>().Property(u => u.Perfil).HasDefaltValue("jogador");         
+
+
+            modelBuilder.Entity<Usuario>()
+            .HasMany(e => e.Personagens)
+            .WithOne(e => e.Usuario)
+            .HasForeignKey(e => e.UsuarioId)
+            .IsRequired(false);
         }
 
         protected override void ConfigureConventions(ModelConfigurationBuilder configurationBuilder)
@@ -52,4 +73,5 @@ namespace RpgApi.Data
         }
         
     }
+
 }
--- RpgApi/Models/Personagem.cs
+++ RpgApi/Models/Personagem.cs
@@ -16,5 +16,8 @@ namespace RpgApi.Models
         public int Defesa { get; set; }
         public int Inteligencia { get; set; } 
         public ClasseEnum Classe { get; set; }
+        public byte[]? FotoPersonagem{get;set;}
+        public int? UsuarioId{get;set;}
+        public Usuario? Usuario {get;set;}
     }
 }
