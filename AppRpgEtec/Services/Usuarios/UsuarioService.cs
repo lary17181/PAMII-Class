8af183be3d9f5bb686472bd8d267d11234475702
unknown <limajeffersonn654@gmail.com>
2025-05-12 08:59:10 -0300

    vs 2022

---
+++ AppRpgEtec/Services/Usuarios/UsuarioService.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using AppRpgEtec.Models;
+
+namespace AppRpgEtec.Services.Usuarios
+{
+    public class UsuarioService : Request
+    {
+        private readonly Request _request;
+        private const string apiUrlBase = "http://xyz.azurewebsites.net/Usuarios";
+
+        public UsuarioService()
+        {
+            //ctor + tab = Construtor
+            _request= new Request();
+        }
+        public async Task<Usuario> PostRegistrarUsuarioAsync(Usuario u)
+        {
+            string urlComplementar = "/Registrar";
+            u.Id = await _request.PostReturnIntAsync(apiUrlBase + urlComplementar, u, string.Empty);
+
+            return u;
+        }
+        public async Task<Usuario> PostAutenticarUsuarioAsync(Usuario u)
+        {
+            string urlComplementar = "/Autenticar";
+            u = await _request.PostAsync(apiUrlBase, u, string.Empty);
+
+            return u;
+        }
+    }
+}
