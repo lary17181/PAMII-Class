d8eb3096ac7bfb92cfcb0aa1f4ebecf33be88e33
coletti <matheus.coletti08@gmail.com>
2025-09-08 08:59:29 -0300

    Aula 6

--- AppRpgEtec/AppRpgEtec/MainPage.xaml.cs
+++ AppRpgEtec/AppRpgEtec/MainPage.xaml.cs
@@ -13,12 +13,7 @@
         {
             count++;
 
-            if (count == 1)
-                CounterBtn.Text = $"Clicked {count} time";
-            else
-                CounterBtn.Text = $"Clicked {count} times";
-
-            SemanticScreenReader.Announce(CounterBtn.Text);
+ 
         }
     }
 
